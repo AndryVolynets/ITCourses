26214fe5d37af320f58c8d268f84e8788552d38e
Andrii Volynets <volinec449@gmail.com>
2023-02-10 23:48:18 +0200

    initial commit

---
+++ resources/views/Home/main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+    <div id="wrapper">
+        <header></header>
+        <nav></nav>
+        <div id="heading"></div>
+        <aside></aside>
+        <section></section>
+    </div>
+    <footer></footer>
+</body>
+
+</html>
